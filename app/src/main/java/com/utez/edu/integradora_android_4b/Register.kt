15b75a9721a718b0810193a4d2485a5b875d8495
maxmendoza <20193tn156@utez.edu.mx>
2021-12-10 01:44:53 -0600

    Intente modificar unas cosas del register para implementar lo de camara pero tendria que cambiar a java casi todo el proyecto, mañana lo arreglo y lo trato de implementar con Kotlin de un video que encontre.

--- app/src/main/java/com/utez/edu/integradora_android_4b/Register.kt
+++ app/src/main/java/com/utez/edu/integradora_android_4b/Register.kt
@@ -2,11 +2,12 @@ package com.utez.edu.integradora_android_4b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
+
 import android.view.View
 import android.widget.Button
-import android.content.Intent
-import android.provider.MediaStore
+
+import android.widget.ImageView
+
 
 
 class Register : AppCompatActivity(), View.OnClickListener {
@@ -15,14 +16,20 @@ class Register : AppCompatActivity(), View.OnClickListener {
         setContentView(R.layout.activity_register)
 
        val btnCamera = findViewById<Button>(R.id.btnCamera)
+       val cam_visor = findViewById<ImageView>(R.id.cam_visor)
 
-        btnCamera.setOnClickListener(this)
+
 
     }
 
-    override fun onClick(p0: View?) {
+    override fun onClick(v: View?) {
+        TODO("Not yet implemented")
+    }
+
+    /*override fun onClick(p0: View?) {
         val cameraIntent = Intent(
             MediaStore.ACTION_IMAGE_CAPTURE
         )
     }
+    */
 }
