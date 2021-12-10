b07b85fe15a20f508d9ca95caf6aade284addedd
maxmendoza <20193tn156@utez.edu.mx>
2021-12-09 23:24:34 -0600

    base del proyecto xd

---
+++ app/src/main/java/com/utez/edu/integradora_android_4b/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.utez.edu.integradora_android_4b
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
