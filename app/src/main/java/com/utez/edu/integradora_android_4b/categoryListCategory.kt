c72bf951fc5ae80dcbec97068feeefafc23b2ca2
maxmendoza <20193tn156@utez.edu.mx>
2021-12-15 00:07:57 -0600

    Me quiero arrancar el pito

--- app/src/main/java/com/utez/edu/integradora_android_4b/categoryListCategory.kt
+++ app/src/main/java/com/utez/edu/integradora_android_4b/categoryListCategory.kt
@@ -2,7 +2,8 @@ package com.utez.edu.integradora_android_4b
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Toast
+import android.util.Log
+import android.widget.*
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
@@ -15,9 +16,74 @@ class categoryListCategory : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_category_list_category)
         supportActionBar?.setDisplayHomeAsUpEnabled(true)
-//        val cola = Volley.newRequestQueue(this@categoryListCategory)
-//        val url = "http://172.17.64.1:4000/categoria"
-//        val listener = Response.Listener<JSONObject> {response ->
+        val spCategoryList = findViewById<Spinner>(R.id.spCategoryList)
+        val btnListCategory = findViewById<Button>(R.id.btnListCategory)
+        val testeoId = findViewById<TextView>(R.id.testeoId)
+        val cola3 = Volley.newRequestQueue(this)
+        val url2 = "http://172.17.64.1:4000/categoria"
+            val listener = Response.Listener<JSONObject> { response ->
+                val listCategory = response.getJSONArray("listcategory")
+                var datos = mutableListOf<String>()
+                for (i in 0 until listCategory.length()) {
+                    datos.add(
+                        listCategory.getJSONObject(i).getString("name")
+                    )
+
+                }
+                val adaptador = ArrayAdapter(
+                    this,
+                    android.R.layout.simple_list_item_1,
+                    datos
+                )
+                spCategoryList.adapter = adaptador
+            }
+            val error = Response.ErrorListener { error ->
+                Log.e("ERROR", error.message.toString())
+            }
+            val peticion = JsonObjectRequest(
+                Request.Method.GET,
+                url2, null, listener, error
+            )
+
+            cola3.add(peticion)
+
+        btnListCategory.setOnClickListener(){
+            val testeoTesteo = spCategoryList.selectedItemId + 1
+            val cola = Volley.newRequestQueue(this)
+            var url3 = "http://172.17.64.1:4000/categoria/categoria/" + testeoTesteo
+            val listener = Response.Listener<JSONObject> {response ->
+
+                val listProducts = response.getJSONArray("cat")
+                var datos = mutableListOf<Producto>()
+
+                for (i in 0 until listProducts.length()){
+                    datos.add(
+                        Producto(
+                            listProducts.getJSONObject(i).getInt("id"),
+                            listProducts.getJSONObject(i).getString("name"),
+                            listProducts.getJSONObject(i).getString("descripcion"),
+                            listProducts.getJSONObject(i).getInt("categoria"),
+                            listProducts.getJSONObject(i).getInt("cantidad"),
+                            listProducts.getJSONObject(i).getString("codigo")
+                        )
+                    )
+                }
+
+                val adaptador = CustomAdapter(this,R.layout.activity_categoria, datos)
+                listView.adapter = adaptador;
+            }
+
+            val error = Response.ErrorListener { error ->
+                Toast.makeText(this,error.message.toString(),Toast.LENGTH_SHORT).show()
+            }
+
+            val peticion = JsonObjectRequest(Request.Method.GET,url3,null,listener,error)
+            cola.add(peticion)
+            url3 = "http://172.17.64.1:4000/categoria/categoria/"
+            }
+
+
+        }
 //            val listProducts = response.getJSONArray("products")
 //            var datosCat = mutableListOf<Categoria>()
 //
@@ -43,4 +109,3 @@ class categoryListCategory : AppCompatActivity() {
 //
 
     }
-}
